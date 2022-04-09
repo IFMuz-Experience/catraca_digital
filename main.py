7b8e7fbc9b9816b6ec27379f8e15fca89f72e8ca
JN513 <jn5.guape@gmail.com>
2022-04-08 21:52:50 -0300

    black .

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import imutils
 import socketio
 import time
 from pyzbar import pyzbar
+from gpio_control import *
 
+WS_URL = "https://1992-200-131-11-6.ngrok.io/"
+SEND_TOPIC = "get-in"
+RECEIVE_TOPIC = "return-in"
 
 cap = cv2.VideoCapture(0)
 
@@ -12,66 +16,89 @@ last_code = ""
 
 
 def release_ratchet():
-	...
+    print("Acesso permitido.")
+
 
 def access_denied():
-	...
+    print("Acesso negado.")
+
 
 @sio.event
 def connect():
     print("I'm connected!")
 
+
 @sio.event
 def connect_error(data):
     print("The connection failed!")
+
 
 @sio.event
 def disconnect():
     print("I'm disconnected!")
 
-@sio.on("return-in")
+
+@sio.on(RECEIVE_TOPIC)
 def on_message(data):
-	print(data)
+    print(data)
 
-def make_request( code ):
-	sio.emit("get-in", {'ra': code})
-	
+
+def make_request(code):
+    sio.emit(SEND_TOPIC, {"ra": code})
+
+
 def main():
-	while True:
+    while True:
 
-		is_ok, img = cap.read()
-		
-		if is_ok:
-			
-			img = imutils.resize(img, 400)
-			
-			qrcodes = pyzbar.decode(img)
-			
-			for qrcode in qrcodes:
-				(x, y, w, h) = qrcode.rect
-				cv2.rectangle(img, (x,y), (x+w,y+h), (0, 255, 0), 2)
-				
-				qrcodedata = qrcode.data.decode("utf-8")
-				qrcodetype = qrcode.type
-				
-				text = f"{qrcodedata}"
-				
-				make_request(text)
-				
-				cv2.putText(img, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-				
-			
-			cv2.imshow("qrcode detector", img)
-			if cv2.waitKey(1) == ord("q"):
-				break
-		else:
-			print("Erro ao iniciar captura.")
-			break
+        is_ok, img = cap.read()
+
+        if is_ok:
+
+            img = imutils.resize(img, 400)
+
+            qrcodes = pyzbar.decode(img)
+
+            for qrcode in qrcodes:
+                (x, y, w, h) = qrcode.rect
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                qrcodedata = qrcode.data.decode("utf-8")
+                qrcodetype = qrcode.type
+
+                text = f"{qrcodedata}"
+
+                if qrcodedata != last_code:
+                    last_code = qrcodedata
+                    print(text)
+                    make_request(qrcodedata)
+                else:
+                    time.sleep(1.0)
+
+                cv2.putText(
+                    img,
+                    text,
+                    (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5,
+                    (0, 0, 255),
+                    2,
+                )
+
+            cv2.imshow("qrcode detector", img)
+            if cv2.waitKey(1) == ord("q"):
+                break
+        else:
+            print("Erro ao iniciar captura.")
+            break
+
 
 if __name__ == "__main__":
-	sio.connect('https://1992-200-131-11-6.ngrok.io/')
+    config_gpio()
 
-	main()
+    sio.connect(WS_URL)
+
+    main()
 
 cap.release()
 cv2.destroyAllWindows()
+sio.disconnect()
